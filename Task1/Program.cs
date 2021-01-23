333a3eb6e88ef0f3cc2c4ed5363ac1a67ab32c42
Sergey Grozov <grozoff92@gmail.com>
2021-01-24 01:50:28 +0500

    Добавил поясняющие комментарии

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,21 +6,24 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            // Указываем размерность двумерного массива
             Console.WriteLine("Введите количество строк:");
             int n = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите количество столбцов:");
             int m = int.Parse(Console.ReadLine());
             int[,] array = new int[n, m];
 
+            // Создаем рандомайзер
             Random rndNum = new Random();
             for (var i = 0; i < array.GetLength(0); i++)
             {
                 for (var j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = rndNum.Next(100);
+                    array[i, j] = rndNum.Next(100); // Ограничиваемся целочисленными до 100
                 }
             }
 
+            // Выводим двумерный массив с рандомными числами что бы понять какую диагональ смотреть
             Console.WriteLine(" \nНаш массив:");
             for (var x = 0; x < array.GetLength(0); x++)
             {
@@ -31,6 +34,7 @@ namespace Task1
                 Console.WriteLine();
             }
 
+            // Выводим главную диагональ
             Console.WriteLine(" \nНаша главная диагональ:");
             for (var x = 0; x < array.GetLength(0); x++)
             {

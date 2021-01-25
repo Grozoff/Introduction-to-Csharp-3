8520a31ced0c4356bfbcf5ea0d039b0eb40b8bcc
Sergey Grozov <grozoff92@gmail.com>
2021-01-25 12:16:56 +0500

    Решение задачи 3 с пользовательским вводом

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите любой текст и нажмите Enter:\t");
+            string word = Console.ReadLine();
+            char[] arrayWords = word.ToCharArray();
+
+
+            Console.WriteLine(" \nСлово наоборот:");
+            for(int i = arrayWords.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arrayWords[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
